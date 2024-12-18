1d1c58e814cdbd25e76ec0e34d85d1ff49f21fee
Andrej Siska <andrej.siska1@gmail.com>
2024-12-18 08:35:54 +0100

    Day 18 added binary search for faster runtime

--- day18/solve.py
+++ day18/solve.py
@@ -6,28 +6,21 @@ bytes = [(y, x) for x, y in bytes]
 
 # R = 7
 # C = 7
-# MAX_BLOCKS = 12
+# BYTES_P1 = 12
 
 R = 71
 C = 71
-MAX_BLOCKS = 1024
+BYTES_P1 = 1024
 
 dirs = [ (1, 0), (0, 1), (-1, 0), (0, -1)]
 START = (0, 0)
-END = (R-1, C-1)
+END = (R - 1, C - 1)
 
-
-for i in range(len(bytes)):
-    
+def find_path(i):
     blocked = set(bytes[:i])
-    found_path = False
+    min_cost = float("inf")
     seen = set()
     q = deque()
-
-    # for r in range(R):
-    #     for c in range(C):
-    #         print("#", end="") if (r, c) in blocked else print(".", end="")
-    #     print()
 
     q.append((*START, 0))
     seen.add(START)
@@ -35,19 +28,31 @@ for i in range(len(bytes)):
     while q:
         r, c, cost = q.popleft()
         if (r, c) == END:
-            if i == MAX_BLOCKS:
-                total_p1 = cost
-            found_path = True
+            min_cost = cost
             break
 
         for dr, dc in dirs:
             if 0 <= r + dr < R and 0 <= c + dc < C and (r + dr, c + dc) not in seen and (r + dr, c + dc) not in blocked:
                 seen.add((r + dr, c + dc))
                 q.append((r + dr, c + dc, cost + 1))
+                
+    return min_cost
 
-    if found_path == False:
-        total_p2 = str(bytes[i-1][1]) + "," + str(bytes[i-1][0])
-        break
+# Part 1
+total_p1 = find_path(BYTES_P1)
+
+# Part 2
+low, high = BYTES_P1, len(bytes)
+
+# binary search to find the first blocking byte
+while low <= high:
+    mid = (low + high) // 2
+    if find_path(mid) == float("inf"):
+        high = mid - 1
+    else:
+        low = mid + 1
+
+total_p2 = str(bytes[mid - 1][1]) + "," + str(bytes[mid - 1][0])
 
 
 print(f"2024 Day 18, Part 1 = {total_p1}") 
